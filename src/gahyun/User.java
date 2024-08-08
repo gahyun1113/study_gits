ae91c90c15fc25c9dc3b33d21a1e35912d6a9ef2
gahyun1113 <tlsrkgus1113@gmail.com>
2024-08-08 11:18:18 +0900

    fsdfsf

--- src/gahyun/User.java
+++ src/gahyun/User.java
@@ -14,15 +14,8 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Todo getTodo() {
         return todo;
     }
 
-    public void setTodo(Todo todo) {
-        this.todo = todo;
-    }
 }
